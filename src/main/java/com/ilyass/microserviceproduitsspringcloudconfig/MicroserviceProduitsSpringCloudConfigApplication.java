18ed3f116b8a39254f432699efb7a837e767e9a7
dell <dell@DESKTOP-4P8U8AR>
2025-01-16 00:40:14 +0100

    initllilzing Project

---
+++ src/main/java/com/ilyass/microserviceproduitsspringcloudconfig/MicroserviceProduitsSpringCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package com.ilyass.microserviceproduitsspringcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProduitsSpringCloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceProduitsSpringCloudConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilyass/microserviceproduitsspringcloudconfig/MicroserviceProduitsSpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyass.microserviceproduitsspringcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProduitsSpringCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
